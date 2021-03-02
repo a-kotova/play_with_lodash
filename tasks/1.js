34b377581f91f6dab6969953fb9150322b96ecaa
Anna Kotova <a.annykot@gmail.com>
2021-03-02 11:39:03 +0300

    Tasks 1 and 2

--- tasks/1.js
+++ tasks/1.js
@@ -1,3 +1,12 @@
 // Task 17:
 // Create function which returns array of all section names.
 //
+
+const data = require('../data/shop');
+const _ = require('lodash');
+
+function arrayOfSectionNames (data) {
+  return _.keys(data.sections);
+}
+
+console.log(arrayOfSectionNames(data));
--- tasks/2.js
+++ tasks/2.js
@@ -2,3 +2,11 @@
 // Create function which returns object containing names of categories as keys
 // and count of subcategories inside of category as a value.
 //
+const data = require('../data/shop');
+const _ = require('lodash');
+
+function countSubCategories(data) {
+  return _.mapValues(data.sections, function(section) { return _.keys(section.categories).length })
+}
+
+console.log(countSubCategories(data));
