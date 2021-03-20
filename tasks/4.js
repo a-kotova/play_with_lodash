b205d2ef4d9202a56358a21504d69323f69c0482
Anna Kotova <a.annykot@gmail.com>
2021-03-20 21:24:02 +0300

    Add task 4

--- tasks/4.js
+++ tasks/4.js
@@ -1,3 +1,12 @@
 // Task 1:
 // Create a function which return the number of all subcategories in the shop
 //
+const data = require('../data/shop');
+const _ = require('lodash');
+
+function getNumberOfSubCategories() {
+  let categories = _.map(data.sections, function(section) { return section.categories});
+  return (_.flatten(_.map(_.assign(...categories), function(category) { return _.keys(category.subCategories) }))).length;
+}
+
+console.log(getNumberOfSubCategories());
