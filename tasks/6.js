2eab8807426f766b871ff25fc8043e5fade1f58f
Hanna Kotava <Hanna.Kotava@itechart-group.com>
2021-03-24 15:34:54 +0300

    Add task 6

--- tasks/6.js
+++ tasks/6.js
@@ -2,3 +2,14 @@
 // Create function which returns all non-unique subcategories.
 // Items in returning collection should have only 3 properties: 'ruName', 'subcategoryPageRuHeading', 'path'
 //
+const data = require('../data/shop');
+const _ = require('lodash');
+
+function getNonUniqueSubCategories() {
+  const categories = _.flatten(_.map(data.sections, function(section) { return _.values(section.categories) }));
+  const allSubCategories = _.flatten(_.map(categories, function(category) { return _.values(category.subCategories) }));
+  return _.map(_.filter(allSubCategories, function(subCategory) { return !subCategory.isUnique }),
+    function(nonUniqueSubCategory) { return _.pick(nonUniqueSubCategory, ['ruName', 'subcategoryPageRuHeading', 'path']) });
+}
+
+console.log(getNonUniqueSubCategories());
