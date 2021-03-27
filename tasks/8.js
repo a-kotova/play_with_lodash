3ab6e27f50b75d56400a7326b0a93708e60a0934
Anna Kotova <a.annykot@gmail.com>
2021-03-27 12:09:11 +0300

    Add task 8

--- tasks/8.js
+++ tasks/8.js
@@ -1,3 +1,11 @@
 // Task 5:
 // Create function which returns collection of all tiles in the shop
 //
+const data = require('../data/shop');
+const _ = require('lodash');
+
+function getAllShopTiles() {
+  return _.concat(data.mainTiles, _.flatMap(data.sections, function(shopSection) { return shopSection.tiles }));
+}
+
+console.log(getAllShopTiles());
