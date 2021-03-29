6b2447ca9eec21acf6a0dd38f9f1523625320d69
Hanna Kotava <a.annykot@gmail.com>
2021-03-29 10:26:49 +0300

    Add task 9

--- tasks/9.js
+++ tasks/9.js
@@ -2,3 +2,23 @@
 // Create a function which returns the most frequently used word in subcategories names
 // with length more than 5 characters
 //
+const data = require('../data/shop');
+const _ = require('lodash');
+
+function getFrequentSubCategoryLongerThan5() {
+  const allCategories =  _.flatMap(data.sections, function (shopSection) {
+    return _.values(shopSection.categories);
+  });
+  const allSubCategoriesPerCategory = _.flatMap(allCategories, function(category) {
+    return _.toArray(_.pick(category, ['subCategories']));
+  });
+  const allSubCategories = _.flatMap(allSubCategoriesPerCategory, function(categoryScope) {
+    return _.keys(categoryScope)
+  });
+  const subCategoriesLongerThan5 = _.filter(allSubCategories, function(subCategory) { return subCategory.length > 5 });
+  const subCategoriesUsage = _.toPairs(_.countBy(subCategoriesLongerThan5, function(subCategory) { return subCategory }));
+  return _.last(_.sortBy(subCategoriesUsage, [function(subCategory) { return subCategory[1] }]))[0];
+
+}
+
+console.log(getFrequentSubCategoryLongerThan5());
