ae1c5c7492a68f9e6bf61b31433ea545a4f674d8
Hanna Kotava <a.annykot@gmail.com>
2021-03-30 16:09:21 +0300

    Remake task 9

--- tasks/9.js
+++ tasks/9.js
@@ -1,24 +1,29 @@
-// Task 6:
-// Create a function which returns the most frequently used word in subcategories names
+// Task 9:
+// Create a function which returns the most frequently used word in subcategories 'ruName'
 // with length more than 5 characters
 //
 const data = require('../data/shop');
 const _ = require('lodash');
 
-function getFrequentSubCategoryLongerThan5() {
+function getFrequentRuNameWordForSubCategoriesLongerThan5() {
   const allCategories =  _.flatMap(data.sections, function (shopSection) {
     return _.values(shopSection.categories);
   });
-  const allSubCategoriesPerCategory = _.flatMap(allCategories, function(category) {
-    return _.toArray(_.pick(category, ['subCategories']));
+  const allSubCategories = _.flatMap(allCategories, function(category) {
+    return _.values(category.subCategories);
   });
-  const allSubCategories = _.flatMap(allSubCategoriesPerCategory, function(categoryScope) {
-    return _.keys(categoryScope)
+  const subCategoriesRuNames = _.map(allSubCategories, function(subCategory) {
+    return subCategory.ruName
   });
-  const subCategoriesLongerThan5 = _.filter(allSubCategories, function(subCategory) { return subCategory.length > 5 });
-  const subCategoriesUsage = _.toPairs(_.countBy(subCategoriesLongerThan5, function(subCategory) { return subCategory }));
-  return _.last(_.sortBy(subCategoriesUsage, [function(subCategory) { return subCategory[1] }]))[0];
+  const wordsInRuNames = _.flatMap(subCategoriesRuNames, function(ruName) {
+    return _.split(ruName, ' ');
+  });
+  const wordsInRuNamesLongerThan5 = _.filter(wordsInRuNames, function(word) {
+    return word.length > 5;
+  });
+  const longWordsUsage = _.toPairs(_.countBy(wordsInRuNamesLongerThan5, function(word) { return word }));
 
+  return _.last(_.sortBy(longWordsUsage, [function(word) { return word[1] }]))[0];
 }
 
-console.log(getFrequentSubCategoryLongerThan5());
+console.log(getFrequentRuNameWordForSubCategoriesLongerThan5());
