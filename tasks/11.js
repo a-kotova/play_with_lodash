88f825ce121470795f56fd6b4fc77800bf5b01b6
Hanna Kotava <a.annykot@gmail.com>
2021-04-07 14:17:02 +0300

    Add task 11

--- tasks/11.js
+++ tasks/11.js
@@ -2,3 +2,28 @@
 // Create function which returns specified amount of subcategories of given category.
 // Handle possible exceptions.
 //
+const data = require('../data/shop');
+const _ = require('lodash');
+
+function getSubCategories(givenCategory, subCategoriesNumber) {
+  const categoriesPerSection = _.flatMap(data.sections, function(shopSection) {
+    return shopSection.categories
+  });
+  const subCategoriesPerCategoryPerSection = _.flatMap(categoriesPerSection, function(sectionScope) {
+    return _.mapValues(sectionScope, function(category) {
+      return _.values(category.subCategories)
+    })
+  });
+
+  if (_.some(_.map(subCategoriesPerCategoryPerSection, function(shopSection) { return _.has(shopSection, givenCategory) }))) {
+    const subCategoriesForGivenCategory = _.flatMap(subCategoriesPerCategoryPerSection, function(sectionScope) {
+      return _.flatten(_.values(_.pick(sectionScope, givenCategory)))
+    });
+
+    return _.sampleSize(subCategoriesForGivenCategory, subCategoriesNumber);
+  } else {
+    throw 'Given category does not exist'
+  }
+}
+
+console.log(getSubCategories('CCTV', 1));
