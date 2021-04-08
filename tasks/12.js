6668b9a8705051eaffdf13dfdb7af13cb9023611
s34rching <ivan.katsenya@gmail.com>
2021-04-08 21:14:53 +0300

    Update description of task 12

--- tasks/12.js
+++ tasks/12.js
@@ -1,3 +1,4 @@
 // Task 9:
-// Create function which returns categories names (grouped by their uniqueness) of given section
+// Create function which returns categories ruNames (grouped by their uniqueness within shop subcategories)
+// of given section
 //
