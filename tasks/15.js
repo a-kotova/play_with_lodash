c24681da95e0a258be9979b06b8eb6d1e48df169
Hanna Kotava <a.annykot@gmail.com>
2021-04-14 17:11:45 +0300

    Add task 15

--- tasks/15.js
+++ tasks/15.js
@@ -2,3 +2,17 @@
 // Create a function which returns array of shop section names modified by given algorithm.
 // Algorithm should be provided during function invocation.
 //
+const data = require('../data/shop');
+const _ = require('lodash');
+
+function getModifiedSectionNames(givenAlgorithm) {
+  return _.map(_.keys(data.sections), function(sectionName) {
+    return givenAlgorithm(sectionName);
+  });
+}
+
+function getStringAcronym(string) {
+  return _.toUpper(string.slice(0,4));
+}
+
+console.log(getModifiedSectionNames(getStringAcronym));
