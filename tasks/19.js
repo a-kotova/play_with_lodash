756b28970ea2356e7366b55974d907639e8b560d
Hanna Kotava <a.annykot@gmail.com>
2021-04-15 11:40:44 +0300

    Add task 19

--- tasks/19.js
+++ tasks/19.js
@@ -2,3 +2,14 @@
 // Create function which returns 'shop' object in 3 seconds.
 // Log result of function execution into console.
 //
+const data = require('../data/shop');
+const _ = require('lodash');
+
+function getObjectWithDelay(objectToGet, delay) {
+  function getObject() {
+    console.log(objectToGet);
+  }
+  return setTimeout(getObject, delay);
+}
+
+getObjectWithDelay(data, 3000);
