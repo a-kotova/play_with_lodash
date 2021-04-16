fa840018c178908b427411fa37a680833b8a4bce
Anna Kotova <a.annykot@gmail.com>
2021-04-16 22:20:40 +0300

    Add task 18

--- tasks/18.js
+++ tasks/18.js
@@ -1,3 +1,24 @@
 // Task 15:
 // Create a function which returns category name of given subcategory name
 //
+const data = require('../data/shop');
+const _ = require('lodash');
+
+function getCategoryOfSubCategory(givenSubCategory) {
+  const categoriesPerSection = _.flatMap(data.sections, function (shopSection) {
+    return shopSection.categories;
+  });
+  const subCategoriesPerCategoryPerSection = _.flatMap(categoriesPerSection, function (sectionScope) {
+    return _.mapValues(sectionScope, function (category) {
+      return category.subCategories;
+    })
+  });
+
+ return _.compact(_.flatMap(subCategoriesPerCategoryPerSection, function(section) {
+   return _.keys(_.pickBy(section, function(category) {
+     return _.has(category, givenSubCategory);
+   }))
+ }));
+}
+
+console.log(getCategoryOfSubCategory('headphones'));
