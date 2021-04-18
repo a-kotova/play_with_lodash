5dbc1c875217549abbce10fa3022f361010bf9e1
Anna Kotova <a.annykot@gmail.com>
2021-04-18 23:40:14 +0300

    Add task 20

--- tasks/20.js
+++ tasks/20.js
@@ -4,3 +4,20 @@
 // - 'categories' of each section should be an array
 // - 'subcategories' of each category should be an array
 //
+const data = require('../data/shop');
+const _ = require('lodash');
+
+function getMutatedObject() {
+  const sectionsWithModifiedSubCategories = _.forEach(data.sections, function(shopSection) {
+    return _.forEach(shopSection.categories, function(category) {
+      category.subCategories = _.toArray(category.subCategories);
+    });
+  });
+  const sectionsWithModifiedCategories = _.forEach(sectionsWithModifiedSubCategories, function(shopSection) {
+    shopSection.categories = _.toArray(shopSection.categories);
+  });
+  data.sections = _.toArray(sectionsWithModifiedCategories);
+  return data;
+}
+
+console.log(getMutatedObject());
